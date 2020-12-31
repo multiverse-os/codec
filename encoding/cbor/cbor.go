9ebe2c89f3a4ab7058d6695ad5c2d09cbbb6376f
Your Name <you@example.com>
2020-12-31 09:06:38 -0500

    must be golang 1.2 but got raw working which was not previously

--- encoding/cbor/cbor.go
+++ encoding/cbor/cbor.go
@@ -1,19 +1,17 @@
 package cbor
 
-import (
-	cbor "./cbor"
-)
-
 type DataFormat struct{}
 
 func (DataFormat) Encode(value interface{}) ([]byte, error) {
-	return cbor.Marshal(value)
+	//return Marshal(value)
+	return []byte{}, nil
 }
 
 func (DataFormat) Decode(data []byte, value interface{}) error {
 	//return cbor.Unmarshal(data, &value)
 	//cborData, _ := hex.DecodeString("a46341676504644e616d656543616e6479664f776e65727382644d617279634a6f65644d616c65f4")
-	return cbor.Unmarshal(data, &value)
+	//return Unmarshal(data, &value)
+	return nil
 }
 
 func (DataFormat) String() string { return "cbor" }
--- encoding/raw/raw.go
+++ encoding/raw/raw.go
@@ -1,7 +1,22 @@
 package raw
 
+import (
+	"bytes"
+	"encoding/binary"
+)
+
 type DataFormat struct{}
 
-func (DataFormat) Encode(input interface{}) ([]byte, error)    { return input.([]byte), nil }
-func (DataFormat) Decode(data []byte, value interface{}) error { return nil }
-func (DataFormat) String() string                              { return "raw" }
+func (DataFormat) Encode(input interface{}) ([]byte, error) {
+	//b := *(*[unsafe.Sizeof(value)]byte)(unsafe.Pointer(&value))
+	var buffer bytes.Buffer
+	binary.Write(&buffer, binary.BigEndian, input)
+	return buffer.Bytes(), nil
+}
+
+func (DataFormat) Decode(data []byte, value interface{}) error {
+	binary.Read(bytes.NewBuffer(data[:]), binary.LittleEndian, &value)
+	return nil
+}
+
+func (DataFormat) String() string { return "raw" }
